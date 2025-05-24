f440edede53e2162a13d6b84fb69af1108ab3688
Finley Mwangola <finley.mwachia12@gmail.com>
2025-05-24 08:53:31 +0300

    remove all files

--- metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Add support for NativeWind
-config.transformer.babelTransformerPath = require.resolve('nativewind/dist/babel');
-
-module.exports = config;
